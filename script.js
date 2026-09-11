776367d261e2292f63f9d2d46031e5d46fbedf32
c20245022.pequitpequit@csav.edu.ph <c20245484.develos@csav.edu.ph>
2026-09-11 12:31:37 +0800

    addEventListener

--- script.js
+++ script.js
@@ -63,3 +63,24 @@ heading.style.backgroundColor = "yellow";
 contactHeading.style.color = "purple";
 projectsHeading.style.color = "purple";
 servicesHeading.style.color = "purple";
+
+//Event Listeners
+heading.addEventListener("click", function () {
+    heading.style.color = "red";
+});
+
+const toggleButton = document.querySelector('#switch');
+const body = document.querySelector('body');
+let isOff = false;
+
+toggleButton.addEventListener("click", function () {
+    isOff = !isOff;
+
+    if (isOff) {
+        body.style.backgroundColor = "black";
+    } else {
+        body.style.backgroundColor = "white";
+
+
+    }
+});
