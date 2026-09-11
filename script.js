5ee16aad62b22d4adc31ea4e96122afc004d52b3
c20245022.pequitpequit@csav.edu.ph <c20245484.develos@csav.edu.ph>
2026-09-11 09:54:14 +0800

    First Javasrcript

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("hello World");
+console.log("Hello, world.");
+console.log("First JavaScript");
+
+
+const myName = "Jery A. pequitpequit";
+myNumber = 99704657576,
+    myAdress = "Punta Mesa Manapla";
+
+let age = 20;
+
+console.log("Name: " + myName);
+console.log("Age: " + age);
+console.log("Number: " + myNumber);
+console.log("Address: " + myAdress);
+
+//Functions
+function greet(name) {
+    return "Good morning, ${Jery A. pequitpequuit"
+}
+
+console.log(greet(myName));
