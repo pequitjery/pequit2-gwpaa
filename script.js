53faa9d911ac650553d0313465c911087a0764c3
c20245022.pequitpequit@csav.edu.ph <c20245484.develos@csav.edu.ph>
2026-09-11 10:46:04 +0800

    variables (let & const)

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ console.log("First JavaScript");
 
 
 const myName = "Jery A. pequitpequit";
-myNumber = 99704657576,
-    myAdress = "Punta Mesa Manapla";
+let myNumber = 99704657576;
+let myAdress = "Punta Mesa Manapla";
 
 let age = 20;
 
@@ -14,9 +14,3 @@ console.log("Age: " + age);
 console.log("Number: " + myNumber);
 console.log("Address: " + myAdress);
 
-//Functions
-function greet(name) {
-    return "Good morning, ${Jery A. pequitpequuit"
-}
-
-console.log(greet(myName));
